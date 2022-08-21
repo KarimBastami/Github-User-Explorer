1f0f57919a447688e4caeac4cbb456fc54e455b9
Karim Mohamed Omar <49725155+KarimBastami@users.noreply.github.com>
2022-08-21 02:08:18 +0200

    feat: new footer content

--- github-user-explorer/src/components/layout/Footer.jsx
+++ github-user-explorer/src/components/layout/Footer.jsx
@@ -1,24 +1,25 @@
-import { FaSlackHash, FaLinkedinIn } from "react-icons/fa"
+import { TbBrandGithub } from "react-icons/tb"
+import { FaSlackHash } from "react-icons/fa" 
 
 function Footer() {
 
-  const currentYear = new Date().getFullYear()
 
   return (
     <footer className="footer items-center p-4 bg-gray-700 text-primary-content">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="items-center flex">
-          <FaSlackHash className="text-3xl mr-2"/>
-          <p>Copyright © {currentYear} - All right reserved</p>
+      <div className="container mx-auto flex flex-col items-center">
+        <div className="items-center flex flex-col">
+          <FaSlackHash className="text-4xl"/>
+          <p className="text-lg text-center">Check the project source code "ironically" on</p>
+          <a href="#" 
+             rel="noreferrer" 
+             target="_blank"
+             className="text-lg ml-1 text-accent hover:text-accent-focus    cursor-pointer">
+            <div className="flex flex-row items-center">
+              <TbBrandGithub className="mr-0.5"/>
+              <h3>GITHUB</h3>
+            </div>
+          </a>
         </div>
-
-        <a href="https://linkedin.com/in/karim-bastami" 
-           rel="noreferrer" 
-           target="_blank" 
-           className="btn btn-ghost btn-md">
-            
-          <FaLinkedinIn className="text-2xl"/>
-        </a>
       </div>
     </footer>
   )
